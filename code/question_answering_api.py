85f64de57fb8ba16f9de438b5c1e60b662c4e210
Veselina Zatsepina <v.zatsepina@alpinadigital.ru>
2021-12-15 22:31:40 +0300

    added model api

---
+++ code/question_answering_api.py
@@ -0,0 +1,19 @@
+import question_answering_model as fl
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+
+class Item(BaseModel):
+    questions: set
+    paragraph: str
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "This is a question answering model"
+
+@app.post("/predict/")
+def predict(item: Item):
+    return fl.getAnswer(item.questions, item.paragraph)
+
--- code/question_answering_model.py
+++ code/question_answering_model.py
@@ -3,26 +3,16 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from transformers import BertTokenizer
 
+class Result:
+    def __init__(self, result):
+        self.result = result
 
-def main():
+def getAnswer(questions, paragraph):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     model = hub.load('bert-uncased-tf2-qa_1')
-    questions = ['What\'s the second-largest city of Russia?', 'How much sovereign nations does Russia have?']
-    paragraph = '''<p>Russia (Russian: Россия, Rossiya, Russian pronunciation:
-                    [rɐˈsʲijə]), or the Russian Federation,[b] is a country spanning
-                    Eastern Europe and Northern Asia. It is the largest country in the
-                    world, covering over 17,125,191 square kilometres (6,612,073 sq mi),
-                    and encompassing one-eighth of Earth's inhabitable landmass. Russia
-                    extends across eleven time zones, and has the most borders of any
-                    country in the world, with sixteen sovereign nations.[c] It has a
-                    population of 146.2 million; and is the most populous country in
-                    Europe, and the ninth-most populous country in the world. Moscow,
-                    the capital, is the largest city in Europe; while Saint Petersburg
-                    is the second-largest city and cultural centre. Russians are the
-                    largest Slavic and European nation; they speak Russian, the most
-                    spoken Slavic language, and the most spoken native language in Europe.</p>'''
-
+   
+    res = []
     for question in questions:
         question_tokens = tokenizer.tokenize(question)
         paragraph_tokens = tokenizer.tokenize(paragraph)
@@ -39,9 +29,6 @@ def main():
         short_end = tf.argmax(outputs[1][0][1:]) + 1
         answer_tokens = tokens[short_start: short_end + 1]
         answer = tokenizer.convert_tokens_to_string(answer_tokens)
-        print(f'Question: {question}')
-        print(f'Answer: {answer}')
+        res.append(f'Question: {question} Answer: {answer}')
+    return Result(result = set(res))
 
-
-if __name__ == '__main__':
-    main()
