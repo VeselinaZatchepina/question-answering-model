e167555b7f9ea0d5d457728348b7dacdc7276a20
Veselina <veselinazatchepina@gmail.com>
2021-12-22 23:27:00 +0300

    Update question_answering_api.py

--- question_answering_api.py
+++ question_answering_api.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import tensorflow_hub as hub
 import json
+from unsync import unsync
 
 class Item(BaseModel):
     questions: set
     paragraph: str
         
+model = None
+
 app = FastAPI()
 
 
@@ -19,4 +22,11 @@ async def root():
 def predict(item: Item):
     return fl.getAnswer(item.questions, item.paragraph, model)
 
-model = hub.load('https://github.com/see--/natural-question-answering/releases/download/v0.0.1/model.tar.gz')
+
+
+@unsync
+async def demo_async_fn():
+    global model
+    model = hub.load('https://github.com/see--/natural-question-answering/releases/download/v0.0.1/model.tar.gz')
+
+demo_async_fn().result()
