369c13c09a1f27accc56da4a5ded66907c0a3f73
Veselina <veselinazatchepina@gmail.com>
2021-12-23 00:57:19 +0300

    Update question_answering_model.py

--- question_answering_model.py
+++ question_answering_model.py
@@ -1,32 +1,47 @@
 import os
-import tensorflow as tf
-import tensorflow_hub as hub
-from transformers import BertTokenizer
+import torch
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
-class Result:
-    def __init__(self, result):
-        self.result = result
+def answer_question(question, answer_text):
+    name = "mrm8488/bert-small-finetuned-squadv2"
 
-def getAnswer(questions, paragraph, model):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-   
-    res = []
-    for question in questions:
-        question_tokens = tokenizer.tokenize(question)
-        paragraph_tokens = tokenizer.tokenize(paragraph)
-        tokens = ['[CLS]'] + question_tokens + ['[SEP]'] + paragraph_tokens + ['[SEP]']
-        input_word_ids = tokenizer.convert_tokens_to_ids(tokens)
-        input_mask = [1] * len(input_word_ids)
-        input_type_ids = [0] * (1 + len(question_tokens) + 1) + [1] * (len(paragraph_tokens) + 1)
+    tokenizer = AutoTokenizer.from_pretrained(name,)
 
-        input_word_ids, input_mask, input_type_ids = map(lambda t: tf.expand_dims(
-            tf.convert_to_tensor(t, dtype=tf.int32), 0), (input_word_ids, input_mask, input_type_ids))
-        outputs = model([input_word_ids, input_mask, input_type_ids])
-        # using `[1:]` will enforce an answer. `outputs[:][0][0]` is the ignored '[CLS]' token logit.
-        short_start = tf.argmax(outputs[0][0][1:]) + 1
-        short_end = tf.argmax(outputs[1][0][1:]) + 1
-        answer_tokens = tokens[short_start: short_end + 1]
-        answer = tokenizer.convert_tokens_to_string(answer_tokens)
-        res.append(f'Question: {question} Answer: {answer}')
-    return Result(result = set(res))
+    model = AutoModelForQuestionAnswering.from_pretrained(name)
+    '''
+    Takes a `question` string and an `answer` string and tries to identify 
+    the words within the `answer` that can answer the question. Prints them out.
+    '''
+    
+    # tokenize the input text and get the corresponding indices
+    token_indices = tokenizer.encode(question, answer_text)
+
+    # Search the input_indices for the first instance of the `[SEP]` token.
+    sep_index = token_indices.index(tokenizer.sep_token_id)
+
+    seg_one = sep_index + 1
+
+    # The remainders lie in the second segment.
+    seg_two = len(token_indices) - seg_one
+    
+    # Construct the list of 0s and 1s.
+    segment_ids = [0]*seg_one + [1]*seg_two
+
+    # get the answer for the question
+    start_scores, end_scores = model(torch.tensor([token_indices]), # The tokens representing our input combining question and answer.
+                                    token_type_ids=torch.tensor([segment_ids])) # The segment IDs to differentiate question from answer
+
+    # Find the tokens with the highest `start` and `end` scores.
+    answer_begin = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores)
+
+    # Get the string versions of the input tokens.
+    indices_tokens = tokenizer.convert_ids_to_tokens(token_indices)
+    
+    answer = indices_tokens[answer_begin:answer_end+1]
+    #remove special tokens
+    answer = [word.replace("▁","") if word.startswith("▁") else word for word in answer] #use this when using model "twmkn9/albert-base-v2-squad2"
+    answer = " ".join(answer).replace("[CLS]","").replace("[SEP]","").replace(" ##","")
+    
+    return answer
 
