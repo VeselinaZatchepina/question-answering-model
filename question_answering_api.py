4015c10965b0c52e3816c4b615b51fd07b040e13
Veselina <veselinazatchepina@gmail.com>
2021-12-22 21:08:55 +0300

    Update question_answering_api.py

--- question_answering_api.py
+++ question_answering_api.py
@@ -1,6 +1,7 @@
 import question_answering_model as fl
 from fastapi import FastAPI
 from pydantic import BaseModel
+import tensorflow_hub as hub
 import json
 
 class Item(BaseModel):
@@ -9,11 +10,13 @@ class Item(BaseModel):
 
 app = FastAPI()
 
+model = hub.load('https://github.com/see--/natural-question-answering/releases/download/v0.0.1/model.tar.gz')
+
 @app.get("/")
 async def root():
     return "This is a question answering model"
 
 @app.post("/predict/")
 def predict(item: Item):
-    return fl.getAnswer(item.questions, item.paragraph)
+    return fl.getAnswer(item.questions, item.paragraph, model)
 
